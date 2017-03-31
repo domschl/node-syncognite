df6c6f951534fa8a6050e5c3c98d3032dc998993
Dominik Schloesser <dsc@dosc.net>
2017-03-31 19:05:22 +0200

    twitter real time stream added

---
+++ modules/node-syncognite-twitter.js
@@ -0,0 +1,54 @@
+var CLog = require('./node-syncognite-util.js');
+var XE = require('../node-syncognite.js');
+
+// https://www.npmjs.com/package/twitter
+var TW = require('twitter');
+
+function twitterSetEntity(entity, property, val, timestamp) {
+    var msg= {MsgType:"EntityMsg", Entity: entity, Property: property, Value: val, Time: timestamp};
+    XE.ent(msg);
+}
+
+var Twitter = function() {};
+
+Twitter.prototype.init = function(md) {
+    var client = new TW({
+        consumer_key: md['consumer_key'],
+        consumer_secret: md['consumer_secret'],
+        access_token_key: md['access_token_key'],
+        access_token_secret: md['access_token_secret']
+    });
+    
+    var stream = client.stream('statuses/filter', {track: md['Track']});
+    stream.on('data', function(event) {
+        var entity='twitter'
+        var property='tweet'
+        var value=event.text
+        var timestamp=Date.now()/1000.0
+        twitterSetEntity(entity,property,value,timestamp);
+    });
+    
+    stream.on('error', function(error) {
+        throw error;
+    });
+    /*
+    // You can also get the stream in a callback if you prefer. 
+    client.stream('statuses/filter', {track: 'münchen'}, function(stream) {
+        stream.on('data', function(event) {
+            console.log("---Ev----------");
+            console.log(event);
+            console.log("---Ev.text-----");
+            console.log(event.text);
+            console.log("---------------");
+        });
+        
+        stream.on('error', function(error) {
+            console.log("something went wrong!");
+        });
+    });
+    */
+ 
+    XE.LogF("syncognite","Twitter","Info","Starting twitter stream");
+}
+
+module.exports = new Twitter();
